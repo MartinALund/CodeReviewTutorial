b3c359950e6d5c22c96a841e1efd4dfc677d9078
Anders Madsen <ande48d0@edu.eal.dk>
2019-04-10 13:06:34 +0200

    +Memes!

--- CleanCoding/CleanCoding/Program.cs
+++ CleanCoding/CleanCoding/Program.cs
@@ -14,7 +14,6 @@ namespace CleanCoding
         * */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello and welcome to the calculator!");
             Console.WriteLine("Calculate down payment: press 1");
             Console.WriteLine("Calculate approximate loan amount: press 2");
             calculate(Console.ReadLine());
